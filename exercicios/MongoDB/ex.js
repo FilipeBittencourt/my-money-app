fd307f1b72d01f21c479db6f0fcf527b5b6e1141
Filipe <filipe@sisnet.com.br>
2017-05-16 16:22:58 -0300

    ex 16-05-2017

--- exercicios/MongoDB/ex.js
+++ exercicios/MongoDB/ex.js
@@ -92,20 +92,74 @@ db.dropDatabase()
 
 ////
 use theglint
-db.users.find()
-db.experiences.find()
+db.users.find().pretty()
+db.experiences.find().pretty()
+db.experiences.find({"startTime" : ISODate("2017-10-05T03:00:00Z"), 'deleted' : 0, 'status': 'published'}).pretty()
+	
+db.notifications.find().pretty()
+
+db.notifications.remove({type: "email"})
+db.notifications.remove({"recipient" : ObjectId("59137c3fe7e6ddd41f105e6d")})
+ 
+
 db.users.remove({email:"fsbvieira@gmail.com"})
+db.users.remove({email:"fsbvieiragame@gmail.com"})
 db.users.remove({email:"filipeicr@hotmail.com"})
 //eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI1OGU3ZmU1NzhlMmEzYzM4MjQxNjhiZTQiLCJleHAiOjE0OTIyMDM3MzU1NjB9.zXjg1ErEQOslnGIIhMJDd15VjjE7XHiSOfDh-SyTFZA
 "token" : "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI1OTAzYTI4Yzc4MGU5Njc0MWYwYWU2NWMiLCJleHAiOjE0OTQwMTUyNDQxOTJ9.n-g59sLj0kBW6VNmjAHvdEyea9M8cswvnH9uajiunI4",
 MKLINK /D "C:\Repositories\Sisnet\theglint-backend\theglint-app-server\node_modules\theglint-core" "C:\Repositories\Sisnet\theglint-backend\theglint-core"
+MKLINK /D "C:\Repositories\Sisnet\theglint-backend\theglint-service\node_modules\theglint-core" "C:\Repositories\Sisnet\theglint-backend\theglint-core"
+
 MKLINK /D "C:\_REPOSITORIES\GIT-SISNET\theglint-backend\theglint-app-server\node_modules\theglint-core" "C:\_REPOSITORIES\GIT-SISNET\theglint-backend\theglint-core"
+MKLINK /D "C:\Repositories\Sisnet\theglint-backend\theglint-service\node_modules\theglint-core" "C:\Repositories\Sisnet\theglint-backend\theglint-backend\theglint-core"
   
 
 //UPDATE com ID
 db.experiences.update({"_id" : ObjectId("59078a1083928580064deff0")},{$set: {status: 'published'}})
-
 db.experiences.remove({"_id" : ObjectId("58f651417b8a61601690c546")})
 
 
+db.users.update({"_id" : ObjectId("59133c16e7e6ddd41f105e6c")},{$set: {"createdOn" : ISODate("2017-05-02T16:13:10.787Z")}})
+db.users.update({  email:"filipeicr@hotmail.com"  },{$set: {"createdOn" : ISODate("2017-05-01T16:13:10.787Z")}})
+db.users.update({  email:"filipe@sisnet.com.br"  },{$set: {"createdOn" : ISODate("2017-05-01T16:13:10.787Z")}})
+
+EQ("="), GTE(">="), GT(">"), LT("<"), LTE("<=");
+
+
+
+
+git clone -b dev/filipe https://github.com/sisnetglobal/theglint-backend.git
+
+
 http://localhost:8000/api/auth/facebook
+
+
+db.experiences.insert({     
+"name": 'Teste23',
+"startTime": "01/05/2017",
+"description": 'description',
+"endTime": "06/05/2017",
+"type": 'spiritual',
+"location": "5883e5e852bcf9d5668076a0",
+"category": "5883e6b69a8a4bd5cce6dbfb",
+"subcategory": "5883e6c89a8a4bd5cce6dbfc",
+"frequence": 'one_time',
+"ticket": [],    
+"images": [],
+"private": false,
+"tickets": [],
+"status": 'published',
+"drinks": [],
+"food": ["58859c304642bb3040cf6126"],
+"links": [],
+"soldOut": false,
+"hideLocationPriorPurchase": false,
+"artists": [],
+"guidelines": [],
+"tier": 'free',
+"flow": ["5883eba70c97ffda83803cc9"],
+"safetyAgreements": [],
+"tags": ["58859c304642bb3040cf6126"],
+"active": true,
+"deleted": 0 
+})
